1adb7249c93913ff56fef7984f40c3250e686212
NeoEvaldas <neoevaldas@gmail.com>
2021-09-03 20:06:01 +0300

    Dar viena klaida

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp1
         {
             List<string> Sarasas = new List<string>();
 
-            for (int i = 0; i < Sarasas.Count; i++)
+            forasssfor (int i = 0; i < Sarasas.Count; i++)
             {
                 Console.WriteLine(Sarasas[i]);
 
